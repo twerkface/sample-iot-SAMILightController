54d5e4cab2c16262dc8cc92d399d756d441bc221
Yujing Wu <yujingwu@gmail.com>
2015-12-31 17:22:46 -0800

    SAMIDE-332 prototype code for smart light tutorial app

---
+++ raspberrypi/smart_light.js
@@ -0,0 +1,128 @@
+var webSocketUrl = "wss://api.samsungsami.io/v1.1/websocket?ack=true";
+var device_id = "fde8715961f84798a841be23480b8ce5";
+var device_token = "06192752a09041f8b7c2878fa460fedc";
+
+var WebSocket = require('ws');
+var isWebSocketReady = false;
+var ws = null;
+
+var gpio = require('rpi-gpio');
+var myPin = 11;// physical pin #
+var myLEDState = 0;
+
+/**
+ * Gets the current time in millis
+ */
+function getTimeMillis(){
+	return parseInt(Date.now().toString());
+}
+
+/**
+ * Create a /websocket connection 
+ */
+function start() {
+	//Create the websocket connection
+        isWebSocketReady = false;
+	ws = new WebSocket(webSocketUrl);
+	ws.on('open', function() {
+                console.log("Websocket connection is open ....");
+		register();
+	});
+	ws.on('message', function(data) {
+ //               console.log("Received message: " + data + '\n');
+                handleRcvMsg(data);
+	});
+	ws.on('close', function() {
+                console.log("Websocket connection is closed ....");
+	});
+
+    gpio.setup(myPin, gpio.DIR_OUT, function(err) {
+       if (err) throw err;
+       myLEDState = false; // default to false after setting up
+       console.log('Setting pin ' + myPin + ' to out succeeded! \n');
+     });
+}
+
+/**
+ * Sends a register message to /websocket endpoint
+ */
+function register(){
+        console.log("Registering device on the websocket connection");
+	try{
+		var registerMessage = '{"type":"register", "sdid":"'+device_id+'", "Authorization":"bearer '+device_token+'", "cid":"'+getTimeMillis()+'"}';
+                console.log('Sending register message ' + registerMessage + '\n');
+		ws.send(registerMessage, {mask: true});
+                isWebSocketReady = true;
+	}
+	catch (e) {
+		console.error('Failed to register messages. Error in registering message: ' + e.toString());
+	}	
+}
+
+
+/**
+ * Handle Actions
+  Received message: 
+  {"type":"action","cts":1451436813630,"ts":1451436813631,
+   "mid":"37e1d61b61b74a3ba962726cb3ef62f1",
+   "sdid":"fde8715961f84798a841be23480b8ce5",
+   "ddid":"fde8715961f84798a841be23480b8ce5",
+   "data":{"actions":[{"name":"setOn","parameters":{}}]},
+   "ddtid":"dtf3cdb9880d2e418f915fb9252e267051","uid":"650a7c8b6ca44730b077ce849af64e90","mv":1}
+ */
+function handleRcvMsg(msg){
+    var msgObj = JSON.parse(msg);
+    if (msgObj.type != "action") return; //Early return;
+
+    var actions = msgObj.data.actions;
+    var actionName = actions[0].name; //assume that there is only one action in actions
+    console.log("The received action is " + actionName);
+    var newState;
+    if (actionName.toLowerCase() == "seton") {
+        newState = 1;
+    }
+    else if (actionName.toLowerCase() == "setoff") {
+        newState = 0;
+    } else {
+        console.log('Do nothing since receiving unrecoganized action ' + actionName);
+        return;
+    }
+    toggleLED(newState);
+}
+
+function toggleLED(value) {
+    gpio.write(myPin, value, function(err) {
+        if (err) throw err;
+        myLEDState = value;
+        console.log('toggleLED: wrote ' + value + ' to pin #' + myPin);
+        sendStateToSami();
+    });
+
+}
+
+/**
+ * Send one message to SAMI
+ */
+function sendStateToSami(){
+	try{
+	    ts = ', "ts": '+getTimeMillis();
+            var data = {
+       			"state": myLEDState
+   	           };
+	    var payload = '{"sdid":"'+device_id+'"'+ts+', "data": '+JSON.stringify(data)+', "cid":"'+getTimeMillis()+'"}';
+	    console.log('Sending payload ' + payload + '\n');
+	    ws.send(payload, {mask: true});
+	} catch (e) {
+	    console.error('Error in sending a message: ' + e.toString() +'\n');
+	}	
+}
+
+/**
+ * All start here
+ */
+
+
+start(); // create websocket connection
+
+
+
---
+++ sample-android-SAMIIoTSimpleController/app/src/main/java/io/samsungsami/example/samiiotsimplecontroller/MainActivity.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright (C) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.samsungsami.example.samiiotsimplecontroller;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+
+public class MainActivity  extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView mLiveStatus;
+    private TextView mDeviceStatus;
+    private TextView mStatusUpdateTime;
+    private TextView mWSStatus;
+    private TextView mWSReceived; // received ack or msg at /websocket end point
+    private Button onBtn;
+    private Button offBtn;
+
+    private static final String WS_HEADER = "WebSocket /websocket: ";
+    private static final String LIVE_HEADER = "WebSocket /live: ";
+    private static final String DEVICE_REGISTERED = "device registered ";
+    private static final String CONNECTED = "connected ";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView deviceName;
+        TextView deviceID;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLiveStatus = (TextView)findViewById(R.id.live_status);
+        deviceName = (TextView)findViewById(R.id.device_name);
+        deviceID = (TextView)findViewById(R.id.device_id);
+        mDeviceStatus = (TextView)findViewById(R.id.device_status);
+        mStatusUpdateTime = (TextView)findViewById(R.id.status_update_time);
+
+        //YWU TODO add mWSStatus
+        mWSStatus = (TextView)findViewById(R.id.ws_status);
+        mWSReceived = (TextView)findViewById(R.id.ws_received);
+
+        setTitle(R.string.device_monitor_title);
+
+        SAMISession.getInstance().setContext(this);
+        deviceID.setText("Device ID: " + SAMISession.getInstance().getDeviceID());
+        deviceName.setText("Device Name: " + SAMISession.getInstance().getDeviceName());
+
+        onBtn = (Button)findViewById(R.id.onBtn);
+        onBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Log.v(TAG, ": on button is clicked.");
+                    SAMISession.getInstance().sendOnActionInDeviceChannelWS();
+                } catch (Exception e) {
+                    Log.v(TAG, "Run into Exception");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        offBtn = (Button)findViewById(R.id.offBtn);
+        offBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Log.v(TAG, ": off button is clicked.");
+                    SAMISession.getInstance().sendOffActionInDeviceChannelWS();
+                } catch (Exception e) {
+                    Log.v(TAG, "Run into Exception");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mWSUpdateReceiver,
+                makeWebsocketUpdateIntentFilter());
+        mLiveStatus.setText("Connecting to /live ... ");
+        SAMISession.getInstance().connectFirehoseWS();//non blocking
+        mWSStatus.setText("Connecting to /websocket ...");
+        SAMISession.getInstance().connectDeviceChannelWS();//non blocking
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SAMISession.getInstance().disconnectFirehoseWS();
+        SAMISession.getInstance().disconnectDeviceChannelWS();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mWSUpdateReceiver);
+    }
+
+
+    private static IntentFilter makeWebsocketUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(SAMISession.WEBSOCKET_LIVE_ONOPEN);
+        intentFilter.addAction(SAMISession.WEBSOCKET_LIVE_ONMSG);
+        intentFilter.addAction(SAMISession.WEBSOCKET_LIVE_ONCLOSE);
+        intentFilter.addAction(SAMISession.WEBSOCKET_LIVE_ONERROR);
+        intentFilter.addAction(SAMISession.WEBSOCKET_WS_ONOPEN);
+        intentFilter.addAction(SAMISession.WEBSOCKET_WS_ONREG);
+        intentFilter.addAction(SAMISession.WEBSOCKET_WS_ONMSG);
+        intentFilter.addAction(SAMISession.WEBSOCKET_WS_ONACK);
+        intentFilter.addAction(SAMISession.WEBSOCKET_WS_ONCLOSE);
+        intentFilter.addAction(SAMISession.WEBSOCKET_WS_ONERROR);
+        return intentFilter;
+    }
+
+    private final BroadcastReceiver mWSUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (SAMISession.WEBSOCKET_LIVE_ONOPEN.equals(action)) {
+                displayLiveStatus(LIVE_HEADER + CONNECTED);
+            } else if (SAMISession.WEBSOCKET_LIVE_ONMSG.equals(action)) {
+                String status = intent.getStringExtra(SAMISession.DEVICE_DATA);
+                String updateTime = intent.getStringExtra(SAMISession.TIMESTEP);
+                displayDeviceStatus(status, updateTime);
+            } else if (SAMISession.WEBSOCKET_LIVE_ONCLOSE.equals(action) ||
+                    SAMISession.WEBSOCKET_LIVE_ONERROR.equals(action)) {
+                displayLiveStatus(LIVE_HEADER + intent.getStringExtra("error"));
+            } else  if (SAMISession.WEBSOCKET_WS_ONOPEN.equals(action)) {
+                displayWSStatus(WS_HEADER + CONNECTED);
+            } else  if (SAMISession.WEBSOCKET_WS_ONREG.equals(action)) {
+                displayWSStatus(WS_HEADER + DEVICE_REGISTERED);
+            } else if (SAMISession.WEBSOCKET_WS_ONMSG.equals(action)) {
+                displayWSReceived(intent.getStringExtra(("msg"))); //YWU TODO convert to constant
+            } else if (SAMISession.WEBSOCKET_WS_ONACK.equals(action)) {
+                displayWSReceived(intent.getStringExtra(("ack"))); //YWU TODO convert to constant
+            } else if (SAMISession.WEBSOCKET_WS_ONCLOSE.equals(action) ||
+                    SAMISession.WEBSOCKET_WS_ONERROR.equals(action)) {
+                displayWSStatus(WS_HEADER + intent.getStringExtra("error")); //YWU TODO convert to constant
+            }
+
+        }
+    };
+
+    private void displayLiveStatus(String status) {
+        Log.d(TAG, status);
+        mLiveStatus.setText(status);
+    }
+
+    private void displayDeviceStatus(String status, String updateTimems) {
+        mDeviceStatus.setText(status);
+        long time_ms = Long.parseLong(updateTimems);
+        mStatusUpdateTime.setText(DateFormat.getDateTimeInstance().format(new Date(time_ms)));
+    }
+
+    private void displayWSStatus(String status) {
+        Log.d(TAG, status);
+        mWSStatus.setText(status);
+    }
+
+    private void displayWSReceived(String status) {
+        Log.d(TAG, status);
+        mWSReceived.setText(status);
+    }
+
+}
