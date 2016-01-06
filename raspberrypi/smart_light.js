72cd561c7f334387556c15c2404f917197c4fcd1
Yujing Wu <yujingwu@gmail.com>
2016-01-05 17:06:15 -0800

    SAMIDE-332 fix code format (develop IoT control system)

--- raspberrypi/smart_light.js
+++ raspberrypi/smart_light.js
@@ -14,32 +14,32 @@ var myLEDState = 0;
  * Gets the current time in millis
  */
 function getTimeMillis(){
-	return parseInt(Date.now().toString());
+    return parseInt(Date.now().toString());
 }
 
 /**
- * Create a /websocket connection 
+ * Create a /websocket connection and setup GPIO pin
  */
 function start() {
-	//Create the websocket connection
-        isWebSocketReady = false;
-	ws = new WebSocket(webSocketUrl);
-	ws.on('open', function() {
-                console.log("Websocket connection is open ....");
-		register();
-	});
-	ws.on('message', function(data) {
- //               console.log("Received message: " + data + '\n');
-                handleRcvMsg(data);
-	});
-	ws.on('close', function() {
-                console.log("Websocket connection is closed ....");
-	});
+    //Create the WebSocket connection
+    isWebSocketReady = false;
+    ws = new WebSocket(webSocketUrl);
+    ws.on('open', function() {
+        console.log("WebSocket connection is open ....");
+        register();
+    });
+    ws.on('message', function(data) {
+ //      console.log("Received message: " + data + '\n');
+         handleRcvMsg(data);
+    });
+    ws.on('close', function() {
+         console.log("WebSocket connection is closed ....");
+    });
 
     gpio.setup(myPin, gpio.DIR_OUT, function(err) {
-       if (err) throw err;
-       myLEDState = false; // default to false after setting up
-       console.log('Setting pin ' + myPin + ' to out succeeded! \n');
+        if (err) throw err;
+        myLEDState = false; // default to false after setting up
+        console.log('Setting pin ' + myPin + ' to out succeeded! \n');
      });
 }
 
@@ -47,28 +47,32 @@ function start() {
  * Sends a register message to /websocket endpoint
  */
 function register(){
-        console.log("Registering device on the websocket connection");
-	try{
-		var registerMessage = '{"type":"register", "sdid":"'+device_id+'", "Authorization":"bearer '+device_token+'", "cid":"'+getTimeMillis()+'"}';
-                console.log('Sending register message ' + registerMessage + '\n');
-		ws.send(registerMessage, {mask: true});
-                isWebSocketReady = true;
-	}
-	catch (e) {
-		console.error('Failed to register messages. Error in registering message: ' + e.toString());
-	}	
+    console.log("Registering device on the WebSocket connection");
+    try{
+        var registerMessage = '{"type":"register", "sdid":"'+device_id+'", "Authorization":"bearer '+device_token+'", "cid":"'+getTimeMillis()+'"}';
+        console.log('Sending register message ' + registerMessage + '\n');
+        ws.send(registerMessage, {mask: true});
+        isWebSocketReady = true;
+    }
+    catch (e) {
+        console.error('Failed to register messages. Error in registering message: ' + e.toString());
+    }    
 }
 
 
 /**
  * Handle Actions
-  Received message: 
-  {"type":"action","cts":1451436813630,"ts":1451436813631,
+   Example of the received message with Action type:
+
+   {
+   "type":"action","cts":1451436813630,"ts":1451436813631,
    "mid":"37e1d61b61b74a3ba962726cb3ef62f1",
    "sdid":"fde8715961f84798a841be23480b8ce5",
    "ddid":"fde8715961f84798a841be23480b8ce5",
    "data":{"actions":[{"name":"setOn","parameters":{}}]},
-   "ddtid":"dtf3cdb9880d2e418f915fb9252e267051","uid":"650a7c8b6ca44730b077ce849af64e90","mv":1}
+   "ddtid":"dtf3cdb9880d2e418f915fb9252e267051","uid":"650a7c8b6ca44730b077ce849af64e90","mv":1
+   }
+
  */
 function handleRcvMsg(msg){
     var msgObj = JSON.parse(msg);
@@ -104,17 +108,17 @@ function toggleLED(value) {
  * Send one message to SAMI
  */
 function sendStateToSami(){
-	try{
-	    ts = ', "ts": '+getTimeMillis();
-            var data = {
-       			"state": myLEDState
-   	           };
-	    var payload = '{"sdid":"'+device_id+'"'+ts+', "data": '+JSON.stringify(data)+', "cid":"'+getTimeMillis()+'"}';
-	    console.log('Sending payload ' + payload + '\n');
-	    ws.send(payload, {mask: true});
-	} catch (e) {
-	    console.error('Error in sending a message: ' + e.toString() +'\n');
-	}	
+    try{
+        ts = ', "ts": '+getTimeMillis();
+        var data = {
+              "state": myLEDState
+            };
+        var payload = '{"sdid":"'+device_id+'"'+ts+', "data": '+JSON.stringify(data)+', "cid":"'+getTimeMillis()+'"}';
+        console.log('Sending payload ' + payload + '\n');
+        ws.send(payload, {mask: true});
+    } catch (e) {
+        console.error('Error in sending a message: ' + e.toString() +'\n');
+    }    
 }
 
 /**
@@ -122,7 +126,7 @@ function sendStateToSami(){
  */
 
 
-start(); // create websocket connection
+start();
 
 
 
